503a43dfaa9c24448d329c821a4c2df1de98a361
jorgefandinno <jfandinno@unomaha.edu>
2024-01-12 16:31:31 -0600

    update

--- authfmt.py
+++ authfmt.py
@@ -24,6 +24,8 @@ from splitnames import splitname
 
 import data
 
+from bibfmt import _fixdb
+
 NON_ALPHANUMERIC_RE = re.compile(r"[^0-9a-zA-Z]+")
 NON_ALPHANUMERIC_WITH_SPACES_AND_DASHES_RE = re.compile(r"[^ 0-9a-zA-Z-]+")
 NUMERIC_RE = re.compile(r"[0-9]+")
@@ -100,13 +102,13 @@ def cleanup_record(x):
             x[val] = x[val].replace('--', '-')
     return x
 
-def _parser():
+def _parser(customization=cleanup_record):
     '''
     Return a configured bibtex parser.
     '''
     parser = BibTexParser()
     parser.interpolate_strings = False
-    parser.customization = cleanup_record
+    parser.customization = customization
     return parser
 
 def _writer(sorted_entries=True):
@@ -125,23 +127,6 @@ def _writer(sorted_entries=True):
 
 
 
-def join_names_parts(name_parts: dict) -> dict:
-    '''
-    Concatenate the name information into a string.
-    '''
-    d = {}
-    d["first"] = " ".join(name_parts.get("first", []))
-    d["von"] = " ".join(name_parts.get("von", []))
-    d["last"] = " ".join(name_parts.get("last", []))
-    d["jr"] = " ".join(name_parts.get("jr", []))
-    return d
-
-
-
-
-
-
-
 
 
 
@@ -150,7 +135,7 @@ def split_names_to_strs(names: str) -> list[str]:
     '''
     Split the given string containing people names into a list of strings representing the name of each person.
     '''
-    return name.replace("\n", " ").split(' and ')
+    return names.replace("\n", " ").split(' and ')
 
 def split_names_to_dicts(names: str) -> list[dict]:
     '''
@@ -167,18 +152,12 @@ def format_name_dict(name: dict) -> dict:
     '''
     Format name reprented as a dictionary.
     '''
-    if "first" in new_name:
+    if "first" in name:
         new_name = name.copy()
         first_name = " ".join(new_name["first"])
         new_name["first"] = [format_first_name(first_name)]
         return new_name
     return name
-
-def format_names_dict(names: list[dict]) -> list[fict]:
-    '''
-    Format the given list of names represented as dictionaries.
-    '''
-    return [format_name_dict(name) for name in names]
 
 def dict_name_to_str(name: dict) -> str:
     '''
@@ -197,12 +176,12 @@ def dict_name_to_str(name: dict) -> str:
         jr = f" {jr}"
     return f"{first}{von}{last}{jr}"
 
+
 def format_names(names: str) -> str:
     '''
     Format the given string containing people names.
     '''
-    return ' and '.join(dict_name_to_str(format_names_dict(split_names_to_dicts(names))))
-
+    return ' and '.join(dict_name_to_str(format_name_dict(name)) for name in split_names_to_dicts(names))
 
 def format_entry_names(entry):
     '''
@@ -217,69 +196,27 @@ def format_entry_names(entry):
         return entry
     return new_entry
 
+def format_entry(entry):
+    '''
+    Format the given entry.
+    '''
+    new_entry = format_entry_names(cleanup_record(entry))
+    if new_entry == entry:
+        return entry
+    return new_entry
 
-def format_bib_entries(path, *, return_modified=False):
+def format_bib(path):
     '''
     Format the given bibliography file.
     '''
     # read bibliography
     with open(path, "r") as f:
-        db = bp.load(f, _parser())
-
-    existing_ids = set(entry["ID"] for entry in db.entries)
-    modified_people = set() if return_modified else None
-    modified_ids = set() if return_modified else None
-
-    # if procs_db is not None:
-    #     db_dict = {}
-    #     for entry in db.entries:
-    #         id_base = ID_RE.match(entry["ID"])
-    #         if id_base is not None:
-    #             id_base = "".join(id_base.groups()[:2])
-    #             if id_base not in db_dict:
-    #                 db_dict[id_base] = []
-    #             db_dict[id_base].append(entry)
-
-    new_entries = []
-    for entry in db.entries:
-        print(entry["ID"])
-        # if "ID" in entry and entry["ID"] in data.EXCLUDE_IDS:
-        #     new_entries.append(entry)
-        #     continue
-        new_entry = _format_bib_entry(entry, existing_ids, db, None)
-        new_entries.append(new_entry)
-        if modified_ids is not None and new_entry is not entry:
-            modified_ids.add(entry["ID"])
-            # if modified_people is not None:
-            #     modified_people |= modified_people_here
-        # if new_entry is not entry and procs_db is not None:
-        #     id_match = ID_RE.match(new_entry["ID"])
-        #     if id_match is not None:
-        #         id_base = "".join(id_match.groups()[:2])
-        #         if entry["ENTRYTYPE"].lower() != "proceedings" and entry["ID"].startswith("DBLP:") and id_base in db_dict:
-        #             for db_entry in db_dict[id_base]:
-        #                 if _similar_entries(new_entry, db_entry):
-        #                     db_entry_id = db_entry["ID"]
-        #                     new_entry["ID"] = f"REPEATED:{db_entry_id}"
-        #                     break
-        #         if entry["ENTRYTYPE"].lower() == "proceedings" and new_entry["ID"] in procs_db.entries_dict:
-        #             print(_dblp_crossref_to_id(entry["ID"]))
-        #     elif entry["ENTRYTYPE"].lower() == "proceedings" and entry["ID"].startswith("DBLP:"):
-        #         procs_db_entry_id = _dblp_crossref_to_id(entry["ID"]) 
-        #         if procs_db_entry_id in procs_db.entries_dict:
-        #             new_entry["ID"] = f"REPEATED:{procs_db_entry_id}"
-        
-    db.entries = new_entries
+        db = _fixdb(bp.load(f, _parser(customization=format_entry)))
 
     # write the bibliography
-    if not return_modified:
-        print("HERE", path)
-        with open(path, "w") as f:
-            bp.dump(db, f, _writer(sorted_entries=False))
-        return db
-    else:
-        return modified_ids, modified_people
-    
+    with open(path, "w") as f:
+        bp.dump(db, f, _writer(sorted_entries=False))
+   
 
 
 def run():
@@ -315,33 +252,10 @@ def run():
     argcomplete.autocomplete(parser)
     res = parser.parse_args()
     
-    if res.command == "entries":
-        # procs = format_bib_entries('procs.bib')
-        format_bib_entries('krr2.bib')
-        # format_bib_entries('jorge.bib', procs_db=procs)
+    if res.command == "format":
+        format_bib('krr2.bib')
+        # format_bib('procs.bib')
         return 0
-    
-    if res.command == "data":
-        modified_ids1, modified_people1 = format_bib_entries('krr.bib', return_modified=True)
-        modified_ids2, modified_people2 = format_bib_entries('procs.bib', return_modified=True)
-        print("SPLITED_NAMES = {")
-        for x in sorted(modified_people1 | modified_people2):
-            # print(f"    ({x[0]}, {x[1]}, {x[2]}, {x[3]}),")
-            print(f"    {x},")
-        print("}\n\n")
-        print("EXCLUDE_IDS = {")
-        for x in sorted(modified_ids1 | modified_ids2):
-            print(f"    '{x}',")
-        print("}\n\n")
-        return 0
-
-    assert res.command == "check"
-    diff = check_bib('krr.bib') + check_bib('procs.bib')
-    if diff:
-        for x in diff:
-            print(x, file=sys.stderr)
-        return 1
-    return 0
 
 if __name__ == "__main__":
     sys.exit(run())
